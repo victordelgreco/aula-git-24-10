9f5d084f8321e22af00905d5b19970343ac40015
Yago Melo <meloyago@gmail.com>
2022-10-26 18:29:10 -0300

    Adição de função listar e nova propriedade

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const catalogo = require('./users.json')
 
+//Aluno 3 (Yago) - Responsável por criar a função de listar todos os usuários inserindo mais uma informação para cada de ativo: true
+
+catalogo.forEach((usuario) => {
+    usuario["ativo"] = "true";
+    if (usuario.idade > 0){
+      usuario["ativo"] = "true";
+    }
+  });
+  
+  console.log(catalogo)
+
+
 
 //Aluno 5- Responsável por criar a função de remover um usuário
 
 
 
-function remover(codigoId)
+/*function remover(codigoId)
 {
 
     for(let i = 0 ; i < catalogo.length ; i++)
@@ -18,6 +30,9 @@ function remover(codigoId)
              
         }
     }
-}
+}*/
+
+
+
 
 
